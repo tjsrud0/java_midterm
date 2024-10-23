476bab9f2c53d10d5cbb30ce56b6860e0489e484
tjsrud0 <sunkyung040909@gmail.com>
2024-10-23 14:43:28 +0900

    버튼 추가

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,8 +2,16 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionListener;
 
-public class Calculator extends JFrame {
+public class Calculator extends JFrame implements ActionListener{
     private JTextField displayField;
+    private JButton[][] buttons;
+    private String[] buttonLabels = {
+            "1", "2", "3", "+",
+            "4", "5", "6", "-",
+            "7", "8", "9", "/",
+            "C", "0", "x", "=",
+            "CE", "Backspace", ".", ""
+    };
 
     public Calculator() {
         setTitle("Calculator");
@@ -17,10 +25,24 @@ public class Calculator extends JFrame {
         displayField.setFont(new Font("Aral", Font.BOLD, 24));
         add(displayField, BorderLayout.NORTH);
 
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new GridLayout(5, 4));
+        buttons = new JButton[5][4];
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 4; j++) {
+                buttons[i][j] = new JButton(buttonLabels[i * 4 + j]);
+                buttons[i][j].addActionListener(this);
+                buttonPanel.add(buttons[i][j]);
+            }
+        }
+
+        add(buttonPanel, BorderLayout.CENTER);
         setVisible(true);
     }
 
     public static void main(String[] args) {
         new Calculator();
     }
+
 }
